4c31c0b64741d940806c0a7c8c33dad6d72c924e
Joshua Michael Osburn <113357917+Joshthebutt@users.noreply.github.com>
2023-10-24 13:37:41 -0500

    Update install_smartmontools.ps1

--- install_smartmontools.ps1
+++ install_smartmontools.ps1
@@ -20,4 +20,12 @@ else {
 
     Write-Host "Smartmontools has been installed to $installDir."
 }
+$appName = "drive adviser"
+$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq $appName }
 
+if ($app -ne $null) {
+    $app.Uninstall()
+    Write-Host "$appName has been uninstalled."
+} else {
+    Write-Host "$appName is not installed on this system."
+}
