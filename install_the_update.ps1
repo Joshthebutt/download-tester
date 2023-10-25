cfb550c69d3b240071f8c32f29bd6a98b577a493
Joshua Michael Osburn <113357917+Joshthebutt@users.noreply.github.com>
2023-10-25 15:18:53 -0500

    Add files via upload

---
+++ install_the_update.ps1
@@ -0,0 +1,15 @@
+    # Define the download URL for the Smartmontools installer
+    $downloadUrl = "https://driveadviser.com/driveAdviser_remake/download/drive_adviser.msix"
+
+    # Define the installation directory
+    $installDir = "C:\programdata\drive adviser"
+
+    # Download the Smartmontools installer
+    Invoke-WebRequest -Uri $downloadUrl -OutFile "drive_adviser.msix"
+
+    # Install Smartmontools
+    Start-Process -FilePath "drive_adviser.msix" -ArgumentList "/S /D=$installDir" -Wait
+
+
+
+    Write-Host "Update has been installed to $installDir."
