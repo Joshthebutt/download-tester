4194ea57f3a44c538a37d375657e839756151271
Joshua Michael Osburn <113357917+Joshthebutt@users.noreply.github.com>
2023-11-29 09:03:09 -0600

    Create harddrive.ps1

---
+++ harddrive.ps1
@@ -0,0 +1,26 @@
+# Get all physical disks in the system
+$physicalDisks = Get-PhysicalDisk
+
+# Gather information for physical disks
+$physicalDisksInfo = foreach ($disk in $physicalDisks) {
+    $driveLetter = (Get-Partition -DiskNumber $disk.DeviceID | Get-Volume).DriveLetter
+    $model = $disk.Model
+    $serialNumber = $disk.SerialNumber
+
+    [PSCustomObject]@{
+        DriveLetter = $driveLetter
+        Model = $model
+        SerialNumber = $serialNumber
+    }
+}
+
+# Sort disks by drive letter
+$sortedDisks = $physicalDisksInfo | Sort-Object -Property DriveLetter
+
+# Output drive details
+foreach ($disk in $sortedDisks) {
+    "Drive Letter: $($disk.DriveLetter)"
+    "Drive Model: $($disk.Model)"
+    "Drive Serial Number: $($disk.SerialNumber)"
+    "------------"
+}
