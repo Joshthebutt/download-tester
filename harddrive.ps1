56b00127dd92c7363955bdcdbe01de5a92e046f5
Joshua Michael Osburn <113357917+Joshthebutt@users.noreply.github.com>
2023-11-29 10:29:12 -0600

    Update harddrive.ps1

--- harddrive.ps1
+++ harddrive.ps1
@@ -19,8 +19,8 @@ $sortedDisks = $physicalDisksInfo | Sort-Object -Property DriveLetter
 
 # Output drive details
 foreach ($disk in $sortedDisks) {
-    "Drive Letter: $($disk.DriveLetter)"
+    "{Drive Letter: $($disk.DriveLetter)"
     "Drive Model: $($disk.Model)"
-    "Drive Serial Number: $($disk.SerialNumber)"
+    "Drive Serial Number: $($disk.SerialNumber)}"
     "------------"
 }
