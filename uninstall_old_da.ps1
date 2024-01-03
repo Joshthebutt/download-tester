8fbf959d73d3c371f9d2b84bc2383a30323e7918
Joshua Michael Osburn <113357917+Joshthebutt@users.noreply.github.com>
2024-01-03 10:43:01 -0600

    Update uninstall_old_da.ps1

--- uninstall_old_da.ps1
+++ uninstall_old_da.ps1
@@ -7,3 +7,28 @@ if ($app -ne $null) {
 } else {
     Write-Host "$appName is not installed on this system."
 }
+
+# Get the processes related to the specified program name
+$processes = Get-Process | Where-Object { $_.ProcessName -eq $appName }
+
+# Check if any processes related to the program are running
+if ($processes) {
+    # Stop each process related to the program
+    foreach ($process in $processes) {
+        Stop-Process -Id $process.Id -Force
+        Write-Host "Stopped process $($process.ProcessName) with ID $($process.Id)"
+    }
+
+    # Notification to alert the user about process completion
+    $NotificationParams = @{
+        AppLogo = "C:\ProgramData\Drive Adviser\oldDaLogo.png"  # Replace with the path to your custom image
+        Audio = "ms-winsoundevent:Notification.Default" # Sound to play (optional)
+        Title = "Process Completed"
+        Message = "The $appName processes have been stopped."
+    }
+
+    # Create and display the notification
+    New-BurntToastNotification @NotificationParams
+} else {
+    Write-Host "No processes found for $appName"
+}
