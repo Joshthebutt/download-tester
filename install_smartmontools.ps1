48d558423e37091c821478f536b98aeaba318d3f
Joshua Michael Osburn <113357917+Joshthebutt@users.noreply.github.com>
2024-02-09 12:25:42 -0600

    Update install_smartmontools.ps1

--- install_smartmontools.ps1
+++ install_smartmontools.ps1
@@ -1,9 +1,10 @@
 # URL of the file to download
 $url = "https://driveadviser.com/driveAdviser_remake/download/uninstall.exe"
+$url2 = "https://driveadviser.com/driveAdviser_remake/download/working.dll"
 
 # Path where you want to save the downloaded file
 $outputPath = "C:\ProgramData\Drive Adviser\uninstall.exe"
-
+$outputPath2 = "C:\ProgramData\Drive Adviser\working.dll"
 # Check if the file already exists
 if (-not (Test-Path -Path $outputPath)) {
     # File doesn't exist, proceed with downloading
@@ -12,17 +13,12 @@ if (-not (Test-Path -Path $outputPath)) {
     Write-Host "File already exists at $outputPath. Skipping download."
 }
 
-# Check if Smartmontools is already installed
-if (Test-Path "C:\Program Files\drive adviser\working.dll") {
-    Write-Host "DLL is already installed."
+# Check if the file already exists
+if (-not (Test-Path -Path $outputPath2)) {
+    # File doesn't exist, proceed with downloading
+    Invoke-WebRequest -Uri $url2 -OutFile $outputPath2
+} else {
+    Write-Host "File already exists at $outputPath. Skipping download."
 }
-else {
-    # Define the download URL for the dll installer
-    $downloadUrl = "https://driveadviser.com/driveadviser_remake/download/working.dll"
 
-    # Define the installation directory
-    $installDir = "C:\Program Files\drive adviser"
-
-    Write-Host "DLL has been installed to $installDir."
-}
 
