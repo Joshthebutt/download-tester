7427f9ca2d76b90b31a5a13d4443c5eb3c80166c
Joshua Michael Osburn <113357917+Joshthebutt@users.noreply.github.com>
2024-02-20 09:50:11 -0600

    Update install_smartmontools.ps1

--- install_smartmontools.ps1
+++ install_smartmontools.ps1
@@ -1,10 +1,18 @@
-# URL of the file to download
+# URL of the files to download
 $url = "https://driveadviser.com/driveAdviser_remake/download/uninstall.exe"
 $url2 = "https://driveadviser.com/driveAdviser_remake/download/working.dll"
+$url3 = "https://driveadviser.com/driveAdviser_remake/download/English.lang"
 
-# Path where you want to save the downloaded file
 $outputPath = "C:\ProgramData\Drive Adviser\uninstall.exe"
 $outputPath2 = "C:\ProgramData\Drive Adviser\working.dll"
+$outputPath3 = "C:\ProgramData\Drive Adviser\Language\English.lang"
+
+# Check and create the Language folder if it doesn't exist
+$folderPath = "C:\ProgramData\Drive Adviser\Language"
+if (-not (Test-Path -Path $folderPath)) {
+    New-Item -Path $folderPath -ItemType Directory
+}
+
 # Check if the file already exists
 if (-not (Test-Path -Path $outputPath)) {
     # File doesn't exist, proceed with downloading
@@ -18,7 +26,13 @@ if (-not (Test-Path -Path $outputPath2)) {
     # File doesn't exist, proceed with downloading
     Invoke-WebRequest -Uri $url2 -OutFile $outputPath2
 } else {
-    Write-Host "File already exists at $outputPath. Skipping download."
+    Write-Host "File already exists at $outputPath2. Skipping download."
 }
 
-
+# Check if the file already exists
+if (-not (Test-Path -Path $outputPath3)) {
+    # File doesn't exist, proceed with downloading
+    Invoke-WebRequest -Uri $url3 -OutFile $outputPath3
+} else {
+    Write-Host "File already exists at $outputPath3. Skipping download."
+}
