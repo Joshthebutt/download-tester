03b9f4136a8780a18687577823691b4c24d02c19
Joshua Michael Osburn <113357917+Joshthebutt@users.noreply.github.com>
2024-03-06 11:06:12 -0600

    install.ps1

--- install_smartmontools.ps1
+++ install_smartmontools.ps1
@@ -2,10 +2,12 @@
 $url = "https://driveadviser.com/driveAdviser_remake/download/uninstall.exe"
 $url2 = "https://driveadviser.com/driveAdviser_remake/download/working.dll"
 $url3 = "https://driveadviser.com/driveAdviser_remake/download/English.lang"
-
+$url4 = "https://driveadviser.com/driveAdviser_remake/download/uninstall.exe"
 $outputPath = "C:\ProgramData\Drive Adviser\uninstall.exe"
 $outputPath2 = "C:\ProgramData\Drive Adviser\working.dll"
 $outputPath3 = "C:\ProgramData\Drive Adviser\Language\English.lang"
+$outputPath4 = "C:\ProgramData\Drive Adviser\uninstall.exe"
+
 
 # Check and create the Language folder if it doesn't exist
 $folderPath = "C:\ProgramData\Drive Adviser\Language"
@@ -35,4 +37,11 @@ if (-not (Test-Path -Path $outputPath3)) {
     Invoke-WebRequest -Uri $url3 -OutFile $outputPath3
 } else {
     Write-Host "File already exists at $outputPath3. Skipping download."
+}
+# Check if the file already exists
+if (-not (Test-Path -Path $outputPath4)) {
+    # File doesn't exist, proceed with downloading
+    Invoke-WebRequest -Uri $url4 -OutFile $outputPath4
+} else {
+    Write-Host "File already exists at $outputPath4. Skipping download."
 }
